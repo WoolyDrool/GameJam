58d349ad7cf9ca00b1d0f5be86cb3b8677b735a1
WoolyDrool <nikolai.mcneely15@gmail.com>
2020-08-21 16:51:53 -0700

    Uploaded Unity project

---
+++ ECGameJam6/Assets/ChildPickup.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChildPickup : MonoBehaviour
+{
+    public Transform theChild = null;
+    public Transform holdPosition;
+    public Vector3 holdPos;
+    public float range = 3;
+    public bool isHoldingChild = false;
+    public float throwForce = 500;
+    public RaycastHit2D hit2D;
+    public Ray2D ray;
+    public float chargeCounter;
+    float chargeMultiplier;
+    // Start is called before the first frame update
+    void Start()
+    {
+        holdPos = new Vector3(holdPosition.position.x, holdPosition.position.y);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if(Input.GetKeyDown(KeyCode.E) && !isHoldingChild)
+        {
+            hit2D = Physics2D.Raycast(holdPosition.position, holdPosition.TransformDirection(Vector2.up), range);
+            {
+                isHoldingChild = true;
+                theChild = hit2D.transform;
+                theChild.parent = holdPosition;
+                
+                
+            }
+        }
+
+        if(isHoldingChild)
+        {
+            theChild.localPosition = holdPosition.localPosition;
+        }
+        
+        if(Input.GetButtonDown("Fire1") && isHoldingChild)
+        {
+            theChild.GetComponent<Rigidbody2D>().AddForce(holdPosition.TransformDirection(Vector2.up) * throwForce, ForceMode2D.Impulse);
+            Debug.Log("rele");
+            holdPosition.DetachChildren();
+            isHoldingChild = false;
+            theChild = null;
+        }
+    }
+}
---
+++ ECGameJam6/Assets/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed = 15;
+    Vector2 movement;
+    Vector3 controllerMovement;
+    Vector2 mousePos;
+    public Camera cam;
+    
+    void Awake()
+    {
+        cam = Camera.main;
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        
+
+
+        mousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+    }
+    private void FixedUpdate()
+    {
+        movement = transform.rotation * movement;
+        rb.MovePosition(rb.position + movement * speed * Time.fixedDeltaTime);
+        Vector2 lookDir = mousePos - rb.position;
+        float angle = Mathf.Atan2(lookDir.y, lookDir.x) * Mathf.Rad2Deg - 90f;
+        rb.rotation = angle;
+    }
+}
